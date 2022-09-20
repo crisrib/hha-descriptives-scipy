b178ea517228fd5b129331f4f064d985132dcf47
crisrib <103081260+crisrib@users.noreply.github.com>
2022-09-20 15:27:33 -0400

    update

---
+++ descriptives-scipy.py
@@ -0,0 +1,10 @@
+## import packages
+import pandas as pd
+import datetime as dt
+import uuid 
+import numpy as np
+
+## 1. load in data
+df = pd.read_csv('data/School_Learning_Modalities.csv')
+df
+
